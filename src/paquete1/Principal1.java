23ade4e1de4e99b83a60da7fd4f521b67e7e3d7b
Richard <richard19041996@gmail.com>
2023-03-20 17:41:51 +0100

    Cambios_Proyecto1

---
+++ src/paquete1/Principal1.java
@@ -0,0 +1,10 @@
+package paquete1;
+
+public class Principal1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Programa que nos permitira hacer un control de versiones con GIT");
+	}
+
+}
